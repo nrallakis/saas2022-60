10309721ab97452944ba7d9f25ecdb746b57e345
Avgoustinos <avgoustinos.0@gmail.com>
2022-06-30 17:11:50 +0300

    Context added in Sub page

--- frontend/src/pages/subscription_page.tsx
+++ frontend/src/pages/subscription_page.tsx
@@ -1,7 +1,13 @@
+import { useContext } from "react";
 import { Card, Button, Row, Col, Form } from "react-bootstrap";
 import Header from "../components/header";
+import {AuthContext} from "../provider/auth_provider";
 
 function SubscriptionPage() {
+    const user = useContext(AuthContext);
+    const yyyy = user?.lastLogin.getFullYear();
+    const mm = user?.lastLogin.getMonth()! + 1;
+    const dd = user?.lastLogin.getDate();
     return (
         <>
             <Header/>
@@ -17,32 +23,33 @@ function SubscriptionPage() {
                             <Form>
                                 <Form.Group className="w-50 mb-3" controlId="firstName">
                                     <Form.Label>First Name</Form.Label>
-                                    <Form.Control type="name" placeholder="Nicholas"/>
+                                    <Form.Control type="name" placeholder={user?.firstName} disabled/>
                                 </Form.Group>
 
                                 <Form.Group className="w-50 mb-3" controlId="lastName">
                                     <Form.Label>Last Name</Form.Label>
-                                    <Form.Control type="name" placeholder="Rallakis"/>
+                                    <Form.Control type="name" placeholder={user?.lastName} disabled/>
                                 </Form.Group>
                                 <Form.Group className="w-50 mb-3" controlId="email">
                                     <Form.Label>Email address</Form.Label>
-                                    <Form.Control type="email" placeholder="nrallakis@gmail.com"/>
+                                    <Form.Control type="email" placeholder={user?.email} disabled/>
                                 </Form.Group>
 
                                 <Form.Group className="w-50 mb-3" controlId="password">
                                     <Form.Label>Last Login</Form.Label>
-                                    <Form.Control type="password" placeholder="01/03/2022"/>
+                                    <Form.Control type="password" 
+                                        placeholder={`${dd}/${mm}/${yyyy}`} disabled/>
                                 </Form.Group>
                                 <Card>
                                     <Card.Body>
                                         <Row>
                                             <Form.Group className="w-25 mb-3" controlId="password">
                                                 <Form.Label>Days Left</Form.Label>
-                                                <Form.Control type="number" placeholder="15" disabled/>
+                                                <Form.Control type="number" placeholder="15" />
                                             </Form.Group>
                                             <Form.Group className="w-25 mb-3" controlId="password">
                                                 <Form.Label>Extend by (Days)</Form.Label>
-                                                <Form.Control type="number" placeholder="15" disabled/>
+                                                <Form.Control type="number" placeholder="15" />
                                             </Form.Group>
                                         </Row>
                                     </Card.Body>
