14918ac319be45828b8a715d0b85592a57680b50
nrallakis <nrallakis4@gmail.com>
2022-07-12 15:44:17 +0300

    Add api service skeleton

---
+++ frontend/src/data/energy_service.ts
@@ -0,0 +1,33 @@
+import {Points} from "./point";
+
+export interface EnergyService {
+    fetchActualTotalLoad(country: String, start: Date, end: Date): Promise<Points>
+    fetchGenerationPerType(country: String, generationType: String, start: Date, end: Date): Promise<Points>
+    fetchCrossBorderFlows(countryFrom: String, countryTo: String,  start: Date, end: Date): Promise<Points>
+}
+
+const fakeData = [[1167609600000,0.7537],[1167696000000,0.7537],[1167782400000,0.7559],[1167868800000,0.7631],[1167955200000,0.7644],[1168214400000,0.769],[1168300800000,0.7683],[1168387200000,0.77],[1168473600000,0.7703],[1168560000000,0.7757],[1168819200000,0.7728],[1168905600000,0.7721],[1168992000000,0.7748],[1169078400000,0.774],[1169164800000,0.7718],[1169424000000,0.7731],[1169510400000,0.767],[1169596800000,0.769],[1169683200000,0.7706],[1169769600000,0.7752],[1170028800000,0.774]];
+
+export class DataService implements EnergyService {
+    fetchActualTotalLoad(country: String, start: Date, end: Date): Promise<Points> {
+        return Promise.resolve([]);
+    }
+    fetchGenerationPerType(country: String, generationType: String, start: Date, end: Date): Promise<Points> {
+        return Promise.resolve([]);
+    }
+    fetchCrossBorderFlows(countryFrom: String, countryTo: String,  start: Date, end: Date): Promise<Points> {
+        return Promise.resolve([]);
+    }
+}
+
+export class MockDataService implements EnergyService {
+    fetchActualTotalLoad(country: String, start: Date, end: Date): Promise<Points> {
+        return Promise.resolve(fakeData);
+    }
+    fetchGenerationPerType(country: String, generationType: String, start: Date, end: Date): Promise<Points> {
+        return Promise.resolve(fakeData);
+    }
+    fetchCrossBorderFlows(countryFrom: String, countryTo: String,  start: Date, end: Date): Promise<Points> {
+        return Promise.resolve(fakeData);
+    }
+}
