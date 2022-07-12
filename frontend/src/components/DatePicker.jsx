39d12ce8c9e262af5e7840cb437d9ca14745f29a
nrallakis <nrallakis4@gmail.com>
2022-07-12 15:44:39 +0300

    Improve ui of datepicker

--- frontend/src/components/DatePicker.jsx
+++ frontend/src/components/DatePicker.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
-import { render } from "@testing-library/react";
+import React, {useState} from "react";
+import {render} from "@testing-library/react";
 import Calendar from 'react-calendar'; // https://www.npmjs.com/package/react-calendar
 import 'react-calendar/dist/Calendar.css';
 
-export default function DatePicker(){
+export default function DatePicker() {
     const [date, setDate] = useState(new Date());
 
     const onChange = date => {
         setDate(date)
     };
-    return(
-    <div>
-      <Calendar onChange={onChange} value={date} />
-    </div>
+    return (
+        <div>
+            <h4>Date From</h4>
+            <Calendar onChange={onChange} value={date}/>
+            <div className="pb-3"/>
+        </div>
     );
 };
