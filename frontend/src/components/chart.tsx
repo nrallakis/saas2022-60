8428a79868643a4009a2bf6667347b7ee367a2f3
nrallakis <nrallakis4@gmail.com>
2022-07-12 15:43:59 +0300

    Add starting chart

---
+++ frontend/src/components/chart.tsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import HighchartsReact from 'highcharts-react-official';
+import Highcharts from 'highcharts';
+
+class Chart extends React.Component<any, any> {
+    constructor(data: number[][]) {
+        super(data);
+
+        this.state = {
+            // To avoid unnecessary update keep all options in the state.
+            chartOptions: {
+                xAxis: {
+                    categories: ['A', 'B', 'C'],
+                },
+                series: [
+                    { data: [[1,2], [3,2], [4,3]] }
+                ],
+                plotOptions: {
+                    series: {
+                        point: {
+                            events: {
+                                mouseOver: this.setHoverData.bind(this)
+                            }
+                        }
+                    }
+                }
+            },
+            hoverData: null
+        };
+    }
+
+    setHoverData = (e: any) => {
+        // The chart is not updated because `chartOptions` has not changed.
+        this.setState({ hoverData: e.target.category })
+    }
+
+    updateSeries = () => {
+        // The chart is updated only with new options.
+        this.setState({
+            chartOptions: {
+                series: [
+                    { data: [Math.random() * 5, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1]}
+                ]
+            }
+        });
+    }
+
+    render() {
+        const { chartOptions, hoverData } = this.state;
+
+        return (
+            <div>
+                <HighchartsReact
+                    highcharts={Highcharts}
+                    options={chartOptions}
+                />
+                {/*<h3>Hovering over {hoverData}</h3>*/}
+                {/*<button onClick={this.updateSeries.bind(this)}>Update Series</button>*/}
+            </div>
+        )
+    }
+}
+
+export { Chart }
