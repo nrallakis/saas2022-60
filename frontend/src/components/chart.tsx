d92623c33ff8983c3c26c5825fcf7bc1ba6ec072
Nicholas Rallakis <nrallakis4@gmail.com>
2022-07-13 22:22:23 +0300

    Make chart work with data given from props

--- frontend/src/components/chart.tsx
+++ frontend/src/components/chart.tsx
@@ -1,65 +1,91 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import { render } from 'react-dom';
 import HighchartsReact from 'highcharts-react-official';
 import Highcharts from 'highcharts';
+import { Points } from '../data/point';
 
-class Chart extends React.Component<any, any> {
-    constructor(data: number[][]) {
-        super(data);
-
-        this.state = {
-            // To avoid unnecessary update keep all options in the state.
-            chartOptions: {
-                xAxis: {
-                    categories: ['A', 'B', 'C'],
-                },
-                series: [
-                    { data: [[1,2], [3,2], [4,3]] }
-                ],
-                plotOptions: {
-                    series: {
-                        point: {
-                            events: {
-                                mouseOver: this.setHoverData.bind(this)
-                            }
-                        }
-                    }
-                }
-            },
-            hoverData: null
-        };
-    }
-
-    setHoverData = (e: any) => {
-        // The chart is not updated because `chartOptions` has not changed.
-        this.setState({ hoverData: e.target.category })
-    }
-
-    updateSeries = () => {
-        // The chart is updated only with new options.
-        this.setState({
-            chartOptions: {
-                series: [
-                    { data: [Math.random() * 5, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1]}
-                ]
-            }
-        });
-    }
-
-    render() {
-        const { chartOptions, hoverData } = this.state;
-
-        return (
-            <div>
-                <HighchartsReact
-                    highcharts={Highcharts}
-                    options={chartOptions}
-                />
-                {/*<h3>Hovering over {hoverData}</h3>*/}
-                {/*<button onClick={this.updateSeries.bind(this)}>Update Series</button>*/}
-            </div>
-        )
-    }
+export interface ChartProps {
+    data: Points;
 }
+function Chart(props: ChartProps) {
+    const initialState = {
+        chartOptions: {
+            xAxis: {
+                categories: ['A', 'B', 'C'],
+            },
+            series: [
+                { data: props.data }
+            ],
+        },
+        hoverData: null
+    };
+
+    const [state, setState] = useState(initialState);
+    return (
+        <HighchartsReact
+            highcharts={Highcharts}
+            options={state.chartOptions}
+        />
+    );
+}
+
+// class Chart extends React.Component<any, any> {
+//     constructor(data: number[][]) {
+//         super(data);
+
+//         this.state = {
+//             // To avoid unnecessary update keep all options in the state.
+//             chartOptions: {
+//                 xAxis: {
+//                     categories: ['A', 'B', 'C'],
+//                 },
+//                 series: [
+//                     { data: [[1,2], [3,2], [4,3]] }
+//                 ],
+//                 plotOptions: {
+//                     series: {
+//                         point: {
+//                             events: {
+//                                 mouseOver: this.setHoverData.bind(this)
+//                             }
+//                         }
+//                     }
+//                 }
+//             },
+//             hoverData: null
+//         };
+//     }
+
+//     setHoverData = (e: any) => {
+//         // The chart is not updated because `chartOptions` has not changed.
+//         this.setState({ hoverData: e.target.category })
+//     }
+
+//     updateSeries = (data: number[]) => {
+//         // The chart is updated only with new options.
+//         this.setState({
+//             chartOptions: {
+//                 series: [
+//                     { data: data }
+//                 ]
+//             }
+//         });
+//     }
+
+//     render() {
+//         const { chartOptions, hoverData } = this.state;
+
+//         return (
+//             <>
+//                 <HighchartsReact
+//                     highcharts={Highcharts}
+//                     options={chartOptions}
+//                 />
+//                 {/*<h3>Hovering over {hoverData}</h3>*/}
+//                 {/*<button onClick={this.updateSeries.bind(this)}>Update Series</button>*/}
+//             </>
+//         )
+//     }
+// }
 
 export { Chart }
