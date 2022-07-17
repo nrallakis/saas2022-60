ab8d3a09a4a69f8b7674a09fbe9e3b097702167a
Nicholas Rallakis <nrallakis4@gmail.com>
2022-07-17 16:52:40 +0300

    Add all modules to app module, add kafka runner and consumer skeleton

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AtlModule } from './atl/atl.module';
+import { KafkaModule } from './kafka/kafka.module';
+import { GptModule } from './gpt/gpt.module';
+import { FfModule } from './ff/ff.module';
+import { AgptModule } from './agpt/agpt.module';
+import { FfModule } from './ff/ff.module';
 
 @Module({
-  imports: [AtlModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/saas'),
+    AtlModule,
+    KafkaModule,
+    GptModule,
+    FfModule,
+    AgptModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
