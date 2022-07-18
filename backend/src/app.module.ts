348a635edcefbbeee51569dc7a0509ba82a36748
nrallakis <nrallakis4@gmail.com>
2022-07-18 17:53:22 +0300

    Fix forgotten module rename

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,18 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AtlModule } from './atl/atl.module';
 import { KafkaModule } from './kafka/kafka.module';
-import { GptModule } from './gpt/gpt.module';
-import { FfModule } from './ff/ff.module';
 import { AgptModule } from './agpt/agpt.module';
-import { FfModule } from './ff/ff.module';
+import { FFModule } from './ff/ff.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/saas'),
     AtlModule,
     KafkaModule,
-    GptModule,
-    FfModule,
+    FFModule,
     AgptModule,
   ],
   controllers: [AppController],
